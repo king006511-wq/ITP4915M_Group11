afe16a6144a2e07f2652e1a33b9942e300289098
hei0612 <heiby1232@gmail.com>
2026-05-23 16:52:35 +0800

    add logistics

---
+++ ITP4915M_Group11/LogisticsForm.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace ITP4915M_Group11
+{
+    partial class LogisticsForm
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            this.grpPendingOrders = new System.Windows.Forms.GroupBox();
+            this.dgvPendingOrders = new System.Windows.Forms.DataGridView();
+            this.grpDispatchConfig = new System.Windows.Forms.GroupBox();
+            this.lblDispatchID = new System.Windows.Forms.Label();
+            this.txtDispatchID = new System.Windows.Forms.TextBox();
+            this.lblEstDelivery = new System.Windows.Forms.Label();
+            this.dtpEstDelivery = new System.Windows.Forms.DateTimePicker();
+            this.lblMethod = new System.Windows.Forms.Label();
+            this.cboMethod = new System.Windows.Forms.ComboBox();
+            this.lblDriverName = new System.Windows.Forms.Label();
+            this.txtDriverName = new System.Windows.Forms.TextBox();
+            this.btnGenerateDeliveryNote = new System.Windows.Forms.Button();
+            this.grpPendingOrders.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPendingOrders)).BeginInit();
+            this.grpDispatchConfig.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // grpPendingOrders
+            // 
+            this.grpPendingOrders.Controls.Add(this.dgvPendingOrders);
+            this.grpPendingOrders.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold);
+            this.grpPendingOrders.Location = new System.Drawing.Point(16, 15);
+            this.grpPendingOrders.Margin = new System.Windows.Forms.Padding(4);
+            this.grpPendingOrders.Name = "grpPendingOrders";
+            this.grpPendingOrders.Padding = new System.Windows.Forms.Padding(4);
+            this.grpPendingOrders.Size = new System.Drawing.Size(1030, 275);
+            this.grpPendingOrders.TabIndex = 0;
+            this.grpPendingOrders.TabStop = false;
+            this.grpPendingOrders.Text = "1. 待發貨訂單列表 (Pending Orders Ready for Dispatch)";
+            // 
+            // dgvPendingOrders
+            // 
+            this.dgvPendingOrders.AllowUserToAddRows = false;
+            this.dgvPendingOrders.AllowUserToDeleteRows = false;
+            this.dgvPendingOrders.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvPendingOrders.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvPendingOrders.Location = new System.Drawing.Point(12, 31);
+            this.dgvPendingOrders.MultiSelect = false;
+            this.dgvPendingOrders.Name = "dgvPendingOrders";
+            this.dgvPendingOrders.ReadOnly = true;
+            this.dgvPendingOrders.RowHeadersWidth = 51;
+            this.dgvPendingOrders.RowTemplate.Height = 27;
+            this.dgvPendingOrders.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvPendingOrders.Size = new System.Drawing.Size(1005, 227);
+            this.dgvPendingOrders.TabIndex = 0;
+            this.dgvPendingOrders.SelectionChanged += new System.EventHandler(this.dgvPendingOrders_SelectionChanged);
+            // 
+            // grpDispatchConfig
+            // 
+            this.grpDispatchConfig.Controls.Add(this.lblDispatchID);
+            this.grpDispatchConfig.Controls.Add(this.txtDispatchID);
+            this.grpDispatchConfig.Controls.Add(this.lblEstDelivery);
+            this.grpDispatchConfig.Controls.Add(this.dtpEstDelivery);
+            this.grpDispatchConfig.Controls.Add(this.lblMethod);
+            this.grpDispatchConfig.Controls.Add(this.cboMethod);
+            this.grpDispatchConfig.Controls.Add(this.lblDriverName);
+            this.grpDispatchConfig.Controls.Add(this.txtDriverName);
+            this.grpDispatchConfig.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold);
+            this.grpDispatchConfig.Location = new System.Drawing.Point(16, 305);
+            this.grpDispatchConfig.Margin = new System.Windows.Forms.Padding(4);
+            this.grpDispatchConfig.Name = "grpDispatchConfig";
+            this.grpDispatchConfig.Padding = new System.Windows.Forms.Padding(4);
+            this.grpDispatchConfig.Size = new System.Drawing.Size(1030, 160);
+            this.grpDispatchConfig.TabIndex = 1;
+            this.grpDispatchConfig.TabStop = false;
+            this.grpDispatchConfig.Text = "2. 發貨參數設定 (Dispatch Configuration)";
+            // 
+            // lblDispatchID
+            // 
+            this.lblDispatchID.Location = new System.Drawing.Point(20, 42);
+            this.lblDispatchID.Name = "lblDispatchID";
+            this.lblDispatchID.Size = new System.Drawing.Size(220, 25);
+            this.lblDispatchID.TabIndex = 0;
+            this.lblDispatchID.Text = "發貨單號 (Dispatch ID):";
+            // 
+            // txtDispatchID
+            // 
+            this.txtDispatchID.Location = new System.Drawing.Point(245, 39);
+            this.txtDispatchID.Name = "txtDispatchID";
+            this.txtDispatchID.ReadOnly = true;
+            this.txtDispatchID.Size = new System.Drawing.Size(250, 30);
+            this.txtDispatchID.TabIndex = 1;
+            // 
+            // lblEstDelivery
+            // 
+            this.lblEstDelivery.Location = new System.Drawing.Point(540, 42);
+            this.lblEstDelivery.Name = "lblEstDelivery";
+            this.lblEstDelivery.Size = new System.Drawing.Size(220, 25);
+            this.lblEstDelivery.TabIndex = 2;
+            this.lblEstDelivery.Text = "預計送貨日期 (Est Date):";
+            // 
+            // dtpEstDelivery
+            // 
+            this.dtpEstDelivery.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtpEstDelivery.Location = new System.Drawing.Point(765, 39);
+            this.dtpEstDelivery.Name = "dtpEstDelivery";
+            this.dtpEstDelivery.Size = new System.Drawing.Size(240, 30);
+            this.dtpEstDelivery.TabIndex = 3;
+            // 
+            // lblMethod
+            // 
+            this.lblMethod.Location = new System.Drawing.Point(20, 102);
+            this.lblMethod.Name = "lblMethod";
+            this.lblMethod.Size = new System.Drawing.Size(220, 25);
+            this.lblMethod.TabIndex = 4;
+            this.lblMethod.Text = "送貨方式 (Delivery Method):";
+            // 
+            // cboMethod
+            // 
+            this.cboMethod.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboMethod.Location = new System.Drawing.Point(245, 99);
+            this.cboMethod.Name = "cboMethod";
+            this.cboMethod.Size = new System.Drawing.Size(250, 31);
+            this.cboMethod.TabIndex = 5;
+            // 
+            // lblDriverName
+            // 
+            this.lblDriverName.Location = new System.Drawing.Point(540, 102);
+            this.lblDriverName.Name = "lblDriverName";
+            this.lblDriverName.Size = new System.Drawing.Size(220, 25);
+            this.lblDriverName.TabIndex = 6;
+            this.lblDriverName.Text = "司機姓名 (Driver Name):";
+            // 
+            // txtDriverName
+            // 
+            this.txtDriverName.Location = new System.Drawing.Point(765, 99);
+            this.txtDriverName.Name = "txtDriverName";
+            this.txtDriverName.Size = new System.Drawing.Size(240, 30);
+            this.txtDriverName.TabIndex = 7;
+            // 
+            // btnGenerateDeliveryNote
+            // 
+            this.btnGenerateDeliveryNote.BackColor = System.Drawing.Color.MediumSeaGreen;
+            this.btnGenerateDeliveryNote.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnGenerateDeliveryNote.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Bold);
+            this.btnGenerateDeliveryNote.ForeColor = System.Drawing.Color.White;
+            this.btnGenerateDeliveryNote.Location = new System.Drawing.Point(496, 485);
+            this.btnGenerateDeliveryNote.Name = "btnGenerateDeliveryNote";
+            this.btnGenerateDeliveryNote.Size = new System.Drawing.Size(550, 52);
+            this.btnGenerateDeliveryNote.TabIndex = 2;
+            this.btnGenerateDeliveryNote.Text = "生成發貨單與回條 (Generate Delivery Note && Reply Slip)";
+            this.btnGenerateDeliveryNote.UseVisualStyleBackColor = false;
+            this.btnGenerateDeliveryNote.Click += new System.EventHandler(this.btnGenerateDeliveryNote_Click);
+            // 
+            // LogisticsForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1062, 553);
+            this.Controls.Add(this.btnGenerateDeliveryNote);
+            this.Controls.Add(this.grpDispatchConfig);
+            this.Controls.Add(this.grpPendingOrders);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "LogisticsForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "物流管理 - 生成發貨單 (Logistics Processing System)";
+            this.Load += new System.EventHandler(this.LogisticsForm_Load);
+            this.grpPendingOrders.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPendingOrders)).EndInit();
+            this.grpDispatchConfig.ResumeLayout(false);
+            this.grpDispatchConfig.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox grpPendingOrders;
+        private System.Windows.Forms.DataGridView dgvPendingOrders;
+        private System.Windows.Forms.GroupBox grpDispatchConfig;
+        private System.Windows.Forms.Label lblDispatchID;
+        private System.Windows.Forms.TextBox txtDispatchID;
+        private System.Windows.Forms.Label lblEstDelivery;
+        private System.Windows.Forms.DateTimePicker dtpEstDelivery;
+        private System.Windows.Forms.Label lblMethod;
+        private System.Windows.Forms.ComboBox cboMethod;
+        private System.Windows.Forms.Label lblDriverName;
+        private System.Windows.Forms.TextBox txtDriverName;
+        private System.Windows.Forms.Button btnGenerateDeliveryNote;
+    }
+}
--- ITP4915M_Group11/LogisticsForm.cs
+++ ITP4915M_Group11/LogisticsForm.cs
@@ -1,14 +1,157 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Data;
 using System.Windows.Forms;
 
 namespace ITP4915M_Group11
 {
-    public class LogisticsForm : Form
+    public partial class LogisticsForm : Form
     {
+        // Database connection string synchronized with your Form1 setup
+        private string connString = "server=127.0.0.1;database=premium_living_db;user=root;password=;port=3306;SslMode=Disabled;";
+
         public LogisticsForm()
         {
-            this.Text = "Logistics";
-            this.Width = 600;
-            this.Height = 400;
+            InitializeComponent();
+        }
+
+        private void LogisticsForm_Load(object sender, EventArgs e)
+        {
+            SetupDispatchControls();
+            RefreshPendingOrdersGrid();
+        }
+
+        private void SetupDispatchControls()
+        {
+            // Populate delivery method drop-down options as expected by local logistics operations
+            cboMethod.Items.Clear();
+            cboMethod.Items.Add("噸車 / Heavy Truck");
+            cboMethod.Items.Add("客貨車 / Light Van");
+            cboMethod.Items.Add("速遞 / Motorcycle Express");
+            cboMethod.Items.Add("客戶自取 / Self-Pickup");
+            cboMethod.SelectedIndex = 0;
+
+            // Restrict delivery scheduling to current date or future dates to enforce data integrity
+            dtpEstDelivery.MinDate = DateTime.Today;
+        }
+
+        public void RefreshPendingOrdersGrid()
+        {
+            using (MySqlConnection conn = new MySqlConnection(connString))
+            {
+                try
+                {
+                    conn.Open();
+                    // Query orders that have been marked as 'Packed' by the warehouse and are ready for delivery assignment
+                    string query = "SELECT OrderID, CustomerID, OrderDate, Status FROM orders WHERE Status = 'Packed';";
+
+                    using (MySqlDataAdapter adapter = new MySqlDataAdapter(query, conn))
+                    {
+                        DataTable dt = new DataTable();
+                        adapter.Fill(dt);
+                        dgvPendingOrders.DataSource = dt;
+                    }
+
+                    // Apply descriptive bilingual headers onto the dynamically bound schema
+                    if (dgvPendingOrders.Columns.Contains("OrderID")) dgvPendingOrders.Columns["OrderID"].HeaderText = "訂單編號 (Order ID)";
+                    if (dgvPendingOrders.Columns.Contains("CustomerID")) dgvPendingOrders.Columns["CustomerID"].HeaderText = "客戶編號 (Customer ID)";
+                    if (dgvPendingOrders.Columns.Contains("OrderDate")) dgvPendingOrders.Columns["OrderDate"].HeaderText = "訂單日期 (Order Date)";
+                    if (dgvPendingOrders.Columns.Contains("Status")) dgvPendingOrders.Columns["Status"].HeaderText = "狀態 (Status)";
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("加載待出貨訂單時發生錯誤 / Error loading pending orders:\n" + ex.Message, "系統錯誤 (Database Error)", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void dgvPendingOrders_SelectionChanged(object sender, EventArgs e)
+        {
+            // Auto-generate a structured Delivery Note sequence ID based on Section 4.2 of your group's Design Specs
+            if (dgvPendingOrders.SelectedRows.Count > 0)
+            {
+                string trackingSeq = DateTime.Now.ToString("mmssfff");
+                txtDispatchID.Text = "DN-" + DateTime.Now.ToString("yyyy") + "-" + trackingSeq.Substring(0, 5);
+            }
+            else
+            {
+                txtDispatchID.Clear();
+            }
+        }
+
+        private void btnGenerateDeliveryNote_Click(object sender, EventArgs e)
+        {
+            // Test Case Validation: Enforce order row targeting allocation
+            if (dgvPendingOrders.SelectedRows.Count == 0)
+            {
+                MessageBox.Show("請先從列表中選擇一張待發貨的訂單！\nPlease select a pending order from the list.",
+                                "提示 / Prompt", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            // Test Case Validation: Verify that mandatory dispatch parameter logs are filled
+            if (string.IsNullOrWhiteSpace(txtDriverName.Text))
+            {
+                MessageBox.Show("請填寫司機姓名！\nPlease specify the driver's name.",
+                                "輸入錯誤 / Input Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            string selectedOrderID = dgvPendingOrders.SelectedRows[0].Cells["OrderID"].Value.ToString();
+            string deliveryNoteID = txtDispatchID.Text;
+            DateTime estDeliveryDate = dtpEstDelivery.Value;
+            string deliveryMethod = cboMethod.Text;
+            string driverName = txtDriverName.Text.Trim();
+
+            // Transactional multi-table persistence implementation block
+            using (MySqlConnection conn = new MySqlConnection(connString))
+            {
+                try
+                {
+                    conn.Open();
+                    using (MySqlTransaction trans = conn.BeginTransaction())
+                    {
+                        // Action A: Populate the delivery table parameters
+                        string insertSql = @"INSERT INTO delivery_note 
+                                            (DeliveryNoteID, OrderID, EstDeliveryDate, DeliveryMethod, DriverName, Status) 
+                                            VALUES (@dnID, @orderID, @estDate, @method, @driver, 'Dispatched');";
+
+                        using (MySqlCommand cmdInsert = new MySqlCommand(insertSql, conn, trans))
+                        {
+                            cmdInsert.Parameters.AddWithValue("@dnID", deliveryNoteID);
+                            cmdInsert.Parameters.AddWithValue("@orderID", selectedOrderID);
+                            cmdInsert.Parameters.AddWithValue("@estDate", estDeliveryDate.ToString("yyyy-MM-dd"));
+                            cmdInsert.Parameters.AddWithValue("@method", deliveryMethod);
+                            cmdInsert.Parameters.AddWithValue("@driver", driverName);
+                            cmdInsert.ExecuteNonQuery();
+                        }
+
+                        // Action B: Shift Order Transaction status flags inside core sales records
+                        string updateSql = "UPDATE orders SET Status = 'Dispatched' WHERE OrderID = @orderID;";
+                        using (MySqlCommand cmdUpdate = new MySqlCommand(updateSql, conn, trans))
+                        {
+                            cmdUpdate.Parameters.AddWithValue("@orderID", selectedOrderID);
+                            cmdUpdate.ExecuteNonQuery();
+                        }
+
+                        trans.Commit();
+
+                        MessageBox.Show($"發貨憑證生成成功！\nDelivery Note Generated Successfully!\n\n" +
+                                        $"發貨單號 (DN ID): {deliveryNoteID}\n" +
+                                        $"對應訂單 (Order ID): {selectedOrderID}\n\n" +
+                                        $"已自動輸出「送貨單」與「簽收回條」。",
+                                        "操作成功 / Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                        // Refresh component view states
+                        txtDriverName.Clear();
+                        RefreshPendingOrdersGrid();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("儲存物流紀錄時系統出錯 / Transaction execution failed:\n" + ex.Message, "系統錯誤", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
         }
     }
 }
